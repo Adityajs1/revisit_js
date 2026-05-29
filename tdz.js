9cc00066bfb12a2855209762f69664b3be18562d
Adityajs1 <aditya96937@gmail.com>
2026-05-29 17:04:02 +0530

    TDZ

---
+++ tdz.js
@@ -0,0 +1,15 @@
+//TDZ = an area where you can not access a variable until it is initialized
+{
+console.log(address); // tdz started for the add var
+//
+//
+//
+//
+console.log(name); //tdz for name started here
+let name = "Aditya";// went up to here
+//
+//
+let address = "Noida"; // went up to here
+
+
+}
