282d3e6083524387fe99d421ea5a7b6a17a7c5b6
Adityajs1 <aditya96937@gmail.com>
2026-06-02 17:19:47 +0530

    proto

--- functions and objects/prototypes.js
+++ functions and objects/prototypes.js
@@ -24,3 +24,24 @@ Person.prototype.greet = function(){
 }
 const p1 = new Person("Aditya");
 p1.greet();
+
+// Create a Vehicle object with a method describe() that logs "I am a vehicle".
+//Then create a car object with a property brand: "Toyota".
+//Set car's prototype to Vehicle manually (using Object.setPrototypeOf).
+//Call car.describe() — it should work even though car doesn't have that method.
+//Then log car.hasOwnProperty("describe") — should be false.
+//Write it, run it mentally, then share what you get.
+
+const vehicle = {
+    describe(){
+        console.log("I am a Vehicle!");        
+    }
+}
+
+const car = {
+    'brand' : 'suzuki'
+}
+Object.setPrototypeOf(car, vehicle)
+car.describe();
+
+console.log(car.hasOwnProperty("describe"));
