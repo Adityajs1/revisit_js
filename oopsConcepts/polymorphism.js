c8704b3de2c01dbd884b14f9ac29618f007e9c12
Adityajs1 <aditya96937@gmail.com>
2026-06-12 18:10:46 +0530

    poly in oop

---
+++ oopsConcepts/polymorphism.js
@@ -0,0 +1,17 @@
+// Definition: Methods behave differently based on object type.
+// In Code: Override a method in child class.
+
+class Car {
+  drive() {
+    console.log("Driving on fuel");
+  }
+}
+
+class ElectricCar extends Car {
+  drive() {
+    console.log("Driving on electricity");
+  }
+}
+
+const myCars = [new Car(), new ElectricCar()];
+myCars.forEach(car => car.drive());
